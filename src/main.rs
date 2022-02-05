4c631a39af5ff15595e60cb43f6bc868968b6b8c
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-02-05 12:34:57 -0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 
 fn main() {
+    println!("Voting has started. Please vote choice1 or choice2");
+
     println!("Submit vote in the format <your vote> ~ <yoursecret> (example: YES~secret). 
                 You can vote multiple times with different secrets");
 
